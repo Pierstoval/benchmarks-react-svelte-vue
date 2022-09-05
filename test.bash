05db9fd625ed33def6fd3f7892c45a030ddbefb8
Alex Rock Ancelet <pierstoval@gmail.com>
2022-09-05 23:35:26 +0200

    Start writing a script to bench this

---
+++ test.bash
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Cleanup
+rm -rf \
+    react/node_modules \
+    react/build \
+    \
+    react-next/node_modules \
+    react-next/.next \
+    react-next/out \
+    \
+    svelte/node_modules \
+    svelte/dist \
+    \
+    svelte-kit/node_modules \
+    svelte-kit/.svelte-kit \
+    svelte-kit/build

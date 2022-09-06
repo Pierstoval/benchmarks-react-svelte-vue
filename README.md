c838ab1a946eef074d60eca17e7deb6f592a6a89
Alex Rock Ancelet <pierstoval@gmail.com>
2022-09-06 22:52:14 +0200

    Continue

---
+++ README.md
@@ -0,0 +1,1 @@
+A raw benchmark of how much time it takes to install & build React & Svelte apps, and their build size.
--- test.bash
+++ test.bash
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-# Cleanup
+info() {
+    printf " %s" "$1"
+}
+ok() {
+    printf " \033[32m%s\033[0m\n" "Done!"
+}
+
+info "Cleaning up..."
 rm -rf \
     react/node_modules \
     react/build \
@@ -15,3 +22,50 @@ rm -rf \
     svelte-kit/node_modules \
     svelte-kit/.svelte-kit \
     svelte-kit/build
+ok
+
+info "Making sure processtime is installed..."
+cargo install processtime >/dev/null 2>&1
+ok
+
+info "Installing node dependencies..."
+    svelte_yarn=$(processtime --format=ms -- yarn --cwd=svelte 2>&1 | tail -1)
+svelte_kit_yarn=$(processtime --format=ms -- yarn --cwd=svelte-kit 2>&1 | tail -1)
+     react_yarn=$(processtime --format=ms -- yarn --cwd=react 2>&1 | tail -1)
+react_next_yarn=$(processtime --format=ms -- yarn --cwd=react-next 2>&1 | tail -1)
+ok
+
+info "Building projects as static websites..."
+svelte_build=$(processtime --format=ms -- yarn --cwd=svelte build 2>&1 | tail -1)
+svelte_kit_build=$(processtime --format=ms -- yarn --cwd=svelte-kit build 2>&1 | tail -1)
+react_build=$(processtime --format=ms -- yarn --cwd=react build 2>&1 | tail -1)
+react_next_build=$(processtime --format=ms -- yarn --cwd=react-next build 2>&1 | tail -1)
+ok
+
+info "Gathering complete build size..."
+svelte_build_size=$(du -s svelte/dist/ | awk '{print $1}')
+svelte_kit_build_size=$(du -s svelte-kit/build/ | awk '{print $1}')
+react_build_size=$(du -s react/build/ | awk '{print $1}')
+react_next_build_size=$(du -s react-next/out/ | awk '{print $1}')
+ok
+
+info "Results for node dependencies:"
+printf "\n"
+echo " ➡ svelte yarn install:       ${svelte_yarn} ms"
+echo " ➡ svelte_kit yarn install:   ${svelte_kit_yarn} ms"
+echo " ➡ react yarn install:        ${react_yarn} ms"
+echo " ➡ react_next yarn install:   ${react_next_yarn} ms"
+
+info "Results for build time:"
+printf "\n"
+echo " ➡ svelte build time:       ${svelte_build} ms"
+echo " ➡ svelte-kit build time:   ${svelte_kit_build} ms"
+echo " ➡ react build time:        ${react_build} ms"
+echo " ➡ react-next build time:   ${react_next_build} ms"
+
+info "Results for build size:"
+printf "\n"
+echo " ➡ svelte build size:       ${svelte_build_size} bytes"
+echo " ➡ svelte_kit build size:   ${svelte_kit_build_size} bytes"
+echo " ➡ react build size:        ${react_build_size} bytes"
+echo " ➡ react_next build size:   ${react_next_build_size} bytes"

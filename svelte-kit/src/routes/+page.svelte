7370443d696cb3d26177f506f132abced7a0ca86
Alex Rock Ancelet <pierstoval@gmail.com>
2023-02-08 21:53:22 +0100

    Add "todolist" app to svelte kit

--- svelte-kit/src/routes/+page.svelte
+++ svelte-kit/src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import {todoStore} from '$lib/todoStore';
+
+    let input: HTMLElement;
+    let newTodo: string = '';
+
+    function addTodo() {
+        if (!newTodo) {
+            return;
+        }
+        todoStore.update((todos) => [...todos, {text: newTodo}])
+        newTodo = '';
+        input.focus();
+    }
+
+    function removeTodo(todo) {
+        todoStore.update((todos) => todos.filter(i => i !== todo));
+    }
+</script>
+
+<input bind:this={input} type="text" bind:value={newTodo} placeholder="Add a new element"/>
+<button type="button" disabled={newTodo.length === 0} on:click={addTodo}>Add</button>
+
+<ul>
+    {#each $todoStore as todo}
+        <li>
+            {todo.text}
+            <button on:click={() => removeTodo(todo)}>x</button>
+        </li>
+    {/each}
+</ul>

f08c55f90a4d6f06721da27a055ab5b3660289f6
Alex Rock Ancelet <pierstoval@gmail.com>
2023-02-08 21:05:26 +0100

    Update svelte app with a small "todolist"

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -1,45 +1,31 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+    import {todoStore} from './lib/todoStore';
+
+    let input: HTMLElement;
+    let newTodo: string = '';
+
+    function addTodo() {
+        if (!newTodo) {
+            return;
+        }
+        todoStore.update((todos) => [...todos, {text: newTodo}])
+        newTodo = '';
+        input.focus();
+    }
+
+    function removeTodo(todo) {
+        todoStore.update((todos) => todos.filter(i => i !== todo));
+    }
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+<input bind:this={input} type="text" bind:value={newTodo} placeholder="Add a new element"/>
+<button type="button" disabled={newTodo.length === 0} on:click={addTodo}>Add</button>
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+<ul>
+    {#each $todoStore as todo}
+        <li>
+            {todo.text}
+            <button on:click={() => removeTodo(todo)}>x</button>
+        </li>
+    {/each}
+</ul>

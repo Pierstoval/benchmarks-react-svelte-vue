3194718a4d88da9ad4b39f431e07f3305a2a4e15
Alex Rock Ancelet <pierstoval@gmail.com>
2023-08-21 16:58:26 +0200

    Update generate_graph.bash script to use cargo now 🎉

--- generate_graph.bash
+++ generate_graph.bash
@@ -2,6 +2,8 @@
 
 CWD=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 
+cd "$CWD"
+
 #
 # Display functions
 #
@@ -19,9 +21,20 @@ fi
 
 if [[ -z "$OUTPUT_DIR" ]]; then
     err "Please specify the output directory name either as an argument to this script, or with the OUTPUT_DIR environment variable"
+    err "Available values:"
+    output_directories=$(cd "${CWD}/output" && for f in *; do if [ -d "$f" ]; then echo -n "$f " ; fi ; done)
+    err "  $output_directories"
     exit 1
 fi
 
-gnuplot -e "output_dir=\"${OUTPUT_DIR}\"" "${CWD}/graph.gnuplot"
+if [[ ! -f "${CWD}/graphs/target/release/graphs" ]]; then
+    info_ln "First, building the \"graphs\" binary so we can generate graphs:"
+    cargo build --release --manifest-path=graphs/Cargo.toml
+    info_ln "Done!"
+fi
+
+info_ln "Generating graphs from CSV data in \"${CWD}/${OUTPUT_DIR}\""
+
+cargo run --manifest-path=graphs/Cargo.toml -- "$OUTPUT_DIR"
 
 info_ln "Done!"

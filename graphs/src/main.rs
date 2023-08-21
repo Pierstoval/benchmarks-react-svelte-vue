f24fd4a2ab19275f9e29f217fec2a450ef898fde
Alex Rock Ancelet <pierstoval@gmail.com>
2023-08-21 15:39:16 +0200

    Finish graphs for now

--- graphs/src/main.rs
+++ graphs/src/main.rs
@@ -8,19 +8,22 @@ use csv::Reader;
 use plotters::backend::BitMapBackend;
 use plotters::chart::ChartBuilder;
 use plotters::coord::Shift;
-use plotters::style::{BLACK, BLUE, RGBColor};
+use plotters::style::HSLColor;
 use plotters::style::WHITE;
 use plotters::drawing::DrawingArea;
 use plotters::drawing::IntoDrawingArea;
+use plotters::element::Drawable;
 use plotters::element::Circle;
+use plotters::element::PathElement;
+use plotters::prelude::BindKeyPoints;
 use plotters::style::IntoFont;
 use plotters::style::Color;
 use serde::Deserialize;
 
 const OUT_FILE_NAME: &'static str = "graph.png";
-const OUT_IMG_SIZE: (u32, u32) = (1200, 800);
+const OUT_IMG_SIZE: (u32, u32) = (1000, 1500);
 
-type RecordsMap = Vec<(String, (u32, Vec<CsvRecord>))>;
+type RecordsMap = Vec<(String, Vec<CsvRecord>)>;
 
 enum ChartType {
     InstallTime,
@@ -30,7 +33,12 @@ enum ChartType {
     BuildSize,
 }
 
-#[derive(Debug, Deserialize)]
+enum PointDisplayType {
+    Circle,
+    HorizontalLine,
+}
+
+#[derive(Debug, Deserialize, Clone)]
 struct CsvRecord {
     #[serde(skip_deserializing)]
     index: i32,
@@ -50,10 +58,7 @@ fn main() {
     let output_dir: Option<&String> = matches.get_one("output_dir");
     let output_dir = output_dir.unwrap();
 
-    // 2 readers are necessary because they can't be rewinded, and we need 2 loops:
-    // One to calculate maximum values (to allow chart to be correctly positioned).
-    // The other one to actually plot data into the chart.
-    let mut records_map = get_csv_records(output_dir);
+    let records_map = get_csv_records(output_dir);
 
     let root = BitMapBackend::new(OUT_FILE_NAME, OUT_IMG_SIZE)
         .into_drawing_area();
@@ -63,8 +68,13 @@ fn main() {
 
     let max_x = records_map.len() as i32;
 
-    create_chart(&root, &mut records_map, max_x, maximums.install_time, ChartType::InstallTime);
-    // create_chart(&root, &mut records_map, max_x, maximums.build_size, ChartType::BuildSize);
+    let roots = root.split_evenly((5, 1));
+
+    create_chart(&roots.get(0).unwrap(), &records_map, max_x, 0, maximums.install_time, PointDisplayType::HorizontalLine, ChartType::InstallTime);
+    create_chart(&roots.get(1).unwrap(), &records_map, max_x, 0, maximums.build_time, PointDisplayType::HorizontalLine, ChartType::BuildTime);
+    create_chart(&roots.get(2).unwrap(), &records_map, max_x, 0, maximums.build_size, PointDisplayType::Circle, ChartType::BuildSize);
+    create_chart(&roots.get(3).unwrap(), &records_map, max_x, 0, maximums.deps_with_duplicates, PointDisplayType::Circle, ChartType::DepsWithDuplicates);
+    create_chart(&roots.get(4).unwrap(), &records_map, max_x, 0, maximums.deps_without_duplicates, PointDisplayType::Circle, ChartType::DepsWithoutDuplicates);
 
     // To avoid the IO failure being ignored silently, we manually call the present function
     root
@@ -99,19 +109,21 @@ fn get_csv_records(output_dir: &String) -> RecordsMap {
         })
         .collect::<HashMap<String, Reader<File>>>()
         .iter_mut()
-        .enumerate()
-        .map(|(index, (chart_name, csv_reader))| {
-            let chart_name = chart_name.clone();
+        .map(|(app, csv_reader)| {
+            let app = app.clone();
             let records = csv_reader.deserialize().map(|record| {
-                let record: CsvRecord = record.unwrap();
+                let mut record: CsvRecord = record.unwrap();
+                let app_index = apps.clone().iter().position(|r| r.eq(app.as_str())).expect(format!("Could not find app \"{}\".", app).as_str());
+                record.index = app_index as i32 + 1;
                 record
             }).collect::<Vec<CsvRecord>>();
 
-            (chart_name, (index as u32, records))
+            (app, records)
         })
         .collect::<RecordsMap>()
         ;
 
+    // Sort by app name
     records.sort_by(|a, b| a.0.cmp(&b.0));
 
     records
@@ -127,7 +139,7 @@ fn get_maximums(records_map: &RecordsMap) -> CsvRecord {
         build_size: 0,
     };
 
-    for (_chart_name, (_, records)) in records_map.iter() {
+    for (_chart_name, records) in records_map.iter() {
         for record in records {
             if record.install_time > maximums.install_time {
                 maximums.install_time = record.install_time;
@@ -152,74 +164,112 @@ fn get_maximums(records_map: &RecordsMap) -> CsvRecord {
 
 fn create_chart(
     root: &DrawingArea<BitMapBackend, Shift>,
-    csv_records: &mut RecordsMap,
+    csv_records: &RecordsMap,
     max_x: i32,
+    min_y: i32,
     max_y: i32,
+    point_display_type: PointDisplayType,
     chart_type: ChartType,
 ) {
-    let min_x: i32 = 0;
-    let min_y: i32 = 0;
+    // This is necessary because if we only use integer as indices for the X axis,
+    // then plotters will not allow us to put stuff at non-integer coords.
+    // And as float coords do not seem possible, we'll use parts of coords by multiplying by this number:
+    let x_coords_multiplier = 10;
 
-    let max_y = (max_y as f32 * 1.1) as i32;
+    let apps = csv_records.iter().map(|(app, _records)| { app.clone() }).collect::<Vec<String>>();
+    let mut x_key_points = apps.iter().enumerate().map(|(index, _)| x_coords_multiplier * index as i32).collect::<Vec<i32>>();
+    x_key_points.push(x_key_points.last().unwrap() + x_coords_multiplier);
+    let min_x: i32 = 0;
+
+    // Add 5% to max Y to allow the chart to breathe on the top
+    let max_y = (max_y as f32 * 1.05) as i32;
+
+    let number_of_apps = csv_records.len() as f64;
 
     let chart_title = match chart_type {
-        ChartType::InstallTime => "Install time",
-        ChartType::BuildTime => "Build time",
+        ChartType::InstallTime => "Install time (in ms)",
+        ChartType::BuildTime => "Build time (in ms)",
         ChartType::DepsWithDuplicates => "Deps with duplicates",
         ChartType::DepsWithoutDuplicates => "Deps without duplicates",
-        ChartType::BuildSize => "Build size",
+        ChartType::BuildSize => "Build size (in KB)",
     }.to_string();
 
     let mut chart = ChartBuilder::on(&root)
         .x_label_area_size(60)
         .y_label_area_size(60)
-        .margin_bottom(30)
-        .margin_right(20)
-        .caption(chart_title, ("sans-serif", 50.0).into_font())
-        .build_cartesian_2d(min_x..max_x, min_y..max_y)
+        .margin_top(10)
+        .margin_right(15)
+        .margin_bottom(15)
+        .margin_left(15)
+        .caption(chart_title.clone(), ("sans-serif", 50.0).into_font())
+        // "+1" is here to allow the chart to breathe on the right side.
+        .build_cartesian_2d((min_x..((max_x+1)*x_coords_multiplier)).with_key_points(x_key_points), min_y..max_y)
         .unwrap()
     ;
 
-    let iter = csv_records;
+    let mut iter = csv_records.clone();
     iter.sort_by(|a, b| a.0.cmp(&b.0));
 
-    for (chart_name, (index, records)) in iter {
-        let chart_name = chart_name.clone();
+    for (_chart_name, records) in iter {
+        let index = records.get(0).unwrap().index as f64;
+        let percentage = index / number_of_apps;
 
-        let color = match chart_name.as_str() {
-            "angular" => RGBColor(0, 0, 255),
-            "react" => RGBColor(0, 255, 255),
-            "react-vite" => RGBColor(0, 255, 128),
-            "react-next" => RGBColor(0, 255, 0),
-            "svelte" => RGBColor(255, 255, 0),
-            "svelte-kit" => RGBColor(255, 128, 0),
-            "vue" => RGBColor(255, 128, 128),
-            "vue-nuxt" => RGBColor(255, 0, 0),
-            _ => RGBColor(128, 128, 128),
-        }.filled();
+        let color = HSLColor(percentage.into(), 1.0, 0.5).filled();
 
-        chart.draw_series(
-            records.iter().map(|record| {
-                let y_value = match chart_type {
-                    ChartType::InstallTime => record.install_time,
-                    ChartType::BuildTime => record.build_time,
-                    ChartType::DepsWithDuplicates => record.deps_with_duplicates,
-                    ChartType::DepsWithoutDuplicates => record.deps_without_duplicates,
-                    ChartType::BuildSize => record.build_size,
-                };
-                Circle::new((1 + *index as i32, y_value), 5, color)
-            })
-        )
-            .unwrap();
+        let value_function = |record: &CsvRecord| {
+            match chart_type {
+                ChartType::InstallTime => record.install_time,
+                ChartType::BuildTime => record.build_time,
+                ChartType::DepsWithDuplicates => record.deps_with_duplicates,
+                ChartType::DepsWithoutDuplicates => record.deps_without_duplicates,
+                ChartType::BuildSize => record.build_size,
+            }
+        };
+
+        match point_display_type {
+            PointDisplayType::Circle => {
+                chart.draw_series(
+                    records.iter().map(|record| {
+                        let y_value = value_function(record);
+                        Circle::new((x_coords_multiplier * record.index, y_value), 5, color.clone())
+                    })
+                )
+                    .unwrap();
+            }
+            PointDisplayType::HorizontalLine => {
+                chart.draw_series(
+                    records.iter().map(|record| {
+                        let y_value = value_function(record);
+                        PathElement::new(vec![
+                            (x_coords_multiplier * record.index - 3, y_value), // Left
+                            (x_coords_multiplier * record.index + 3, y_value), // Right
+                        ], color.clone())
+                    })
+                )
+                    .unwrap();
+            }
+        };
     }
 
     chart
         .configure_mesh()
         .light_line_style(&WHITE)
-        .x_desc("Application".to_string())
-        .set_all_tick_mark_size(15)
+        .x_label_formatter(&|v: &i32| {
+            let v = v.clone();
+            for (name, records) in csv_records.clone().iter() {
+                let index = records.get(0).unwrap().index * x_coords_multiplier;
+                if index == v {
+                    return name.clone();
+                }
+            }
+            "".into()
+        })
+        .x_desc("Framework".to_string())
+        .x_label_style(("sans-serif", 20))
+        .y_desc(chart_title)
+        .x_label_style(("sans-serif", 20))
+        .set_all_tick_mark_size(5)
         .draw()
         .unwrap()
     ;
-
 }

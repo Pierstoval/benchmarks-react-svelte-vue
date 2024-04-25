9c447e9028e4c35b82c0c53f32afbf5f1e76284e
Alex Rock Ancelet <pierstoval@gmail.com>
2024-04-25 13:18:03 +0200

    Slight improvement on the graph generation script

--- generate_graph.bash
+++ generate_graph.bash
@@ -2,6 +2,8 @@
 
 CWD=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 
+set -e
+
 cd "$CWD"
 
 #
@@ -9,15 +11,17 @@ cd "$CWD"
 #
 
 info_ln() {
-    printf "\033[32m%s\033[0m %s\n" "[INFO]" "$1"
+    printf "\033[32m%s\033[0m ${1}\n" "[INFO]"
 }
 err() {
-    printf " \033[31m[ERROR]\033[0m %s\n" "$1"
+    printf " \033[31m%s\033[0m ${1}\n" "[ERROR]"
 }
 
 if [[ -z "${OUTPUT_DIR}" ]]; then
     OUTPUT_DIR=$1
 fi
+
+set -u
 
 if [[ -z "$OUTPUT_DIR" ]]; then
     err "Please specify the output directory name either as an argument to this script, or with the OUTPUT_DIR environment variable"
@@ -28,9 +32,17 @@ if [[ -z "$OUTPUT_DIR" ]]; then
 fi
 
 info_ln "Building the \"graphs\" binary so we can generate graphs:"
-cargo build --release --manifest-path=graphs/Cargo.toml
+cargo build --release --manifest-path="$PWD/graphs/Cargo.toml"
 info_ln "Done!"
 
-info_ln "Now Generating graphs from CSV data in \"${CWD}/${OUTPUT_DIR}\""
-"${CWD}/graphs/target/release/graphs" "$OUTPUT_DIR"
+TEST_OUT_DIR="${CWD}/output/${OUTPUT_DIR}"
+
+if [[ ! -d "${TEST_OUT_DIR}" ]]; then
+    err "Tests output directory \"${TEST_OUT_DIR}\" does not exist."
+    err "Did you forget to execute the benchmarks with the \"\033[32m%s${CWD}/bench_it_all.bash ${OUTPUT_DIR}\033[0m\" command?"
+    exit 1
+fi
+
+info_ln "Now Generating graphs from CSV data in \"${TEST_OUT_DIR}\""
+"${CWD}/graphs/target/release/graphs" "${OUTPUT_DIR}"
 info_ln "Done!"
